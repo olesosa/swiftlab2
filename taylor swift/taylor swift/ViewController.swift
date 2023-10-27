16937ca8153a845c8ed7eda0803af67b42e8565e
Олесь <102287993+olesosa@users.noreply.github.com>
2023-10-27 11:47:56 +0300

    Add files via upload

---
+++ taylor swift/taylor swift/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  taylor swift
+//
+//  Created by Admin on 22.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
